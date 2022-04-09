af199b6f70a44977abdfc561d7122e767d2f6e88
Albert <allbertt666@gmail.com>
2022-04-08 23:36:55 -0300

    Primeira versão "funcional" de uma Calculadora de algarismos romanos

--- Script.js
+++ Script.js
@@ -1,4 +1,65 @@
-function convertToRoman(num){
+		
+function somar() {
+	let v1 = parseInt(romanoParaDecimal(document.querySelector('#n1').value))
+
+	let v2 = romanoParaDecimal(document.querySelector('#n2').value)
+
+	let v3 = (parseInt(v1)+parseInt(v2))
+
+	let r = document.getElementById('resultado');
+
+	r.innerHTML = parseInt(v3).toString()
+	r.innerHTML ='Resultado:<br/>'+decimalParaRomano(v3).toString()
+	console.log(v3)
+			
+}
+
+function dividir() {
+	let v1 = parseInt(romanoParaDecimal(document.querySelector('#n1').value))
+
+	let v2 = romanoParaDecimal(document.querySelector('#n2').value)
+
+	let v3 = (parseInt(v1)/parseInt(v2))
+
+	let r = document.getElementById('resultado');
+
+	r.innerHTML = parseInt(v3).toString()
+	r.innerHTML = 'Resultado:<br/>'+decimalParaRomano(v3).toString()
+	console.log(v3)
+}
+
+function multiplicar() {
+	let v1 = parseInt(romanoParaDecimal(document.querySelector('#n1').value))
+
+	let v2 = romanoParaDecimal(document.querySelector('#n2').value)
+
+	let v3 = (parseInt(v1)*parseInt(v2))
+
+	let r = document.getElementById('resultado');
+
+	r.innerHTML = parseInt(v3).toString()
+	r.innerHTML = 'Resultado:<br/>'+decimalParaRomano(v3).toString()
+	console.log(v3)
+}
+
+function subtrair() {
+	let v1 = parseInt(romanoParaDecimal(document.querySelector('#n1').value))
+
+	let v2 = romanoParaDecimal(document.querySelector('#n2').value)
+
+	let v3 = (parseInt(v1)-parseInt(v2))
+
+	let r = document.getElementById('resultado');
+
+	r.innerHTML = parseInt(v3).toString()
+	r.innerHTML = 'Resultado:<br/>'+decimalParaRomano(v3).toString()
+	console.log(v3)
+}
+        
+
+
+
+function decimalParaRomano(num){
 	var romanToNum = {
 		M: 1000,
 		CM: 900,
@@ -17,7 +78,7 @@ function convertToRoman(num){
 
 	var roman = "";
 
-	for (var key in romanToNum) {
+	for (let key in romanToNum) {
 		
 		while (num>= romanToNum[key]){
 			roman += key;
@@ -29,4 +90,49 @@ function convertToRoman(num){
 	return roman;
 }
 
-convertToRoman(8);
+	console.log(decimalParaRomano(1200))
+
+	
+
+
+	function romanoParaDecimal(str) {
+		// Roman numeral symbol to number converter
+		const convert = {
+		  I: 1,
+		  V: 5,
+		  X: 10,
+		  L: 50,
+		  C: 100,
+		  D: 500,
+		  M: 1000,
+		};
+	  
+		// initialize the sum
+		let sum = 0;
+	  
+		// the previous symbol value
+		let previous = 0;
+	  
+		// loop through Roman numeral symbols
+		for (let i = 0; i < str.length; i++) {
+		  // get the converted value and handle unknown symbols
+		  const current = convert[str[i]] ?? 0;
+	  
+		  // add the current symbol value
+		  sum += current;
+	  
+		  // if the left-side symbol is a subtractive notation
+		  if (previous < current) {
+			// undo the wrongly added number and subtract the left-side value
+			sum -= previous + previous;
+		  }
+	  
+		  // the current value is the left-side value for the next loop
+		  previous = current;
+		}
+		
+		return sum;
+	  };
+	  
+	  
+	  console.log(romanoParaDecimal('XXX'))
